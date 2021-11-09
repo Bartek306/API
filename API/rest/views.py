fc79a2ef458d578d5cbef84f0bffcd31c3a3fe8e
Bartek306 <shwdp13@wp.pl>
2021-11-09 12:37:10 +0100

    Add views

--- API/rest/views.py
+++ API/rest/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import json
+import requests
+
+from django.http import HttpResponseNotFound, HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+@csrf_exempt
+def string(request):
+    return None
